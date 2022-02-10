c2d995c7dad2317e2243eaa1af688876dc08b71d
ltb <liutianbo@360.cn>
2022-02-10 15:59:44 +0800

    sda

--- js/search.js
+++ js/search.js
@@ -5,8 +5,10 @@
 
 function search() {
     if (document.getElementById("search_input").value != ""){
-        window.location.href="https://so.toutiao.com/search?keyword=" + document.getElementById("search_input").value + "&pd=information&source=search_subtab_switch&dvpf=pc&aid=4916&page_num=0";
-        document.getElementById("search_input").value = "";
+      window.open("https://so.toutiao.com/search?keyword=" + document.getElementById("search_input").value + "&pd=synthesis&source=search_subtab_switch&dvpf=pc&aid=4916&page_num=0")
+      document.getElementById("search_input").value = "";
+    } else {
+      alert("内容不能为空");
     }
 
     return false;
