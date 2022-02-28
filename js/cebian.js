a638c541480478fd545c36e3d2b9bb585a1718d8
ltb <liutianbo@360.cn>
2022-02-28 16:04:54 +0800

    caca

--- js/cebian.js
+++ js/cebian.js
@@ -1,18 +1,18 @@
-$(function () {
-  $(window).scroll(function () {
-    if ($(document).scrollTop() >= 400) {
-      $(".fixedtool").fadeIn();
-    } else {
-      $(".fixedtool").fadeOut();
-    }
-  });
-  $(".fixedtool div").click(function () {
-    // console.log($(this).index());
-    let current = $(".titleAndswiper .toppart .title")
-      .eq($(this).index())
-      .offset().top;
-    $("body, html").stop().animate({
-      screenTop: current,
-    });
-  });
-});
+// $(function () {
+//   $(window).scroll(function () {
+//     if ($(document).scrollTop() >= 400) {
+//       $(".fixedtool").fadeIn();
+//     } else {
+//       $(".fixedtool").fadeOut();
+//     }
+//   });
+//   $(".fixedtool a").click(function () {
+//     // console.log($(this).index());
+//     let current = $(".titleAndswiper .toppart .title")
+//       .eq($(this).index())
+//       .offset().top;
+//     $("body, html").stop().animate({
+//       screenTop: current,
+//     });
+//   });
+// });
