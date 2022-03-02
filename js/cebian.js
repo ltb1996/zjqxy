e97903ec7285ca2d73b1e0c4c44d28b864ea0029
ltb1996 <610030835@qq.com>
2022-03-02 22:47:51 +0800

    sadad

--- js/cebian.js
+++ js/cebian.js
@@ -1,18 +1,50 @@
-// $(function () {
-//   $(window).scroll(function () {
-//     if ($(document).scrollTop() >= 400) {
-//       $(".fixedtool").fadeIn();
-//     } else {
-//       $(".fixedtool").fadeOut();
-//     }
-//   });
-//   $(".fixedtool a").click(function () {
-//     // console.log($(this).index());
-//     let current = $(".titleAndswiper .toppart .title")
-//       .eq($(this).index())
-//       .offset().top;
-//     $("body, html").stop().animate({
-//       screenTop: current,
-//     });
-//   });
-// });
+$(function () {
+    // 当我们点击了小li 此时不需要执行 页面滚动事件里面的 li 的背景选择 添加 current
+    // 节流阀  互斥锁
+    var flag = true;
+    // 1.显示隐藏电梯导航
+    toggleTool();
+  
+    function toggleTool() {
+      if ($(document).scrollTop() >= 200) {
+        $(".cd-vertical-nav").fadeIn();
+      } else {
+        $(".cd-vertical-nav").fadeOut();
+      }
+    }
+  
+    $(window).scroll(function () {
+      toggleTool();
+      // 3. 页面滚动到某个内容区域，左侧电梯导航小li相应添加和删除current类名
+  
+      if (flag) {
+        $(".titleAndswiper").each(function (i, ele) {
+          if ($(document).scrollTop() >= $(ele).offset().top) {
+            console.log(i);
+            $(".cd-vertical-nav li").eq(i).addClass("active").siblings().removeClass();
+          }
+        });
+      }
+    });
+    // 2. 点击电梯导航页面可以滚动到相应内容区域
+    $(".cd-vertical-nav li").click(function () {
+      flag = false;
+      // 当我们每次点击小li 就需要计算出页面要去往的位置
+      // 选出对应索引号的内容区的盒子 计算它的.offset().top
+      var current = $(".titleAndswiper").eq($(this).index()).offset().top;
+      // 页面动画滚动效果
+      $("body, html")
+        .stop()
+        .animate(
+          {
+            scrollTop: current,
+          },
+          function () {
+            flag = true;
+          }
+        );
+      // 点击之后，让当前的小li 添加current 类名 ，姐妹移除current类名
+      $(this).addClass("active").siblings().removeClass();
+    });
+  });
+  
